3098aa7d92673fceab0b35addac7c9e1beda49f7
rafael-bot-fox <106665451+rafael-bot-fox@users.noreply.github.com>
2022-09-17 09:12:07 -0300

    Update nn.js

--- nn.js
+++ nn.js
@@ -1,4 +1,4 @@
-{
+var gg = {
 active: true,
 msg: 'Olá estamos enviando essa msg da central pra avisar que não estamos fazendo vendas nem nada do tipo, caso queira entra em ctt com os criadores entre em ctt:\n\n
 wa.me/5516991865841\n
